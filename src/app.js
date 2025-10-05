d920af811f440b8d6928848833183edea59d12a9
Krishnaraj Thadesar <krishnaraj.kpt@outlook.com>
2025-10-05 16:02:04 +0530

    test

--- src/app.js
+++ src/app.js
@@ -44,7 +44,7 @@ app.use(cors({
 	methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 }));
 
-app.options("/api", (req, res) => {
+app.options("/api/*", (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "https://authentic-tracker.krishnarajthadesar.in");
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
